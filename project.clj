8e500e354294115465d31125370ae6d629ac8caf
Dr. Christian Betz <cb.betz@gmail.com>
2016-05-20 13:25:25 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/eventsourcing "0.1.0"
+(defproject gorillalabs/eventsourcing "0.1.1-SNAPSHOT"
   :description "A Clojure event-sourcing abstraction on configurable storage backends."
   :url "https://github.com/gorillalabs/eventsourcing"
   :license {:name "Eclipse Public License"
